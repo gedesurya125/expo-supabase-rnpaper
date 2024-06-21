d7e3861bf72fa646f4b0ad47cadc2e4fbbb5d2d1
gedesurya125 <gedesurya125@gmail.com>
2024-06-21 10:48:53 +0800

    clean up the home screen and git comment to the default expo component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,59 +1,54 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { StyleSheet, Platform, SafeAreaView } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from "@/components/HelloWave";
+import { ThemedView } from "@/components/ThemedView";
+import { Text } from "react-native-paper";
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+    <SafeAreaView>
+      <ThemedView style={{ height: "100%" }}>
+        <ThemedView style={styles.titleContainer}>
+          <Text variant="displayLarge">Welcome! Surya</Text>
+          <HelloWave />
+        </ThemedView>
+        <ThemedView style={styles.stepContainer}>
+          <Text variant="titleLarge">Step 1: Try it</Text>
+          <Text>
+            Edit <Text variant="bodyLarge">app/(tabs)/index.tsx</Text> to see
+            changes. Press{" "}
+            <Text variant="bodyLarge">
+              {Platform.select({ ios: "cmd + d", android: "cmd + m" })}
+            </Text>{" "}
+            to open developer tools.
+          </Text>
+        </ThemedView>
+        <ThemedView style={styles.stepContainer}>
+          <Text variant="titleLarge">Step 2: Explore</Text>
+          <Text>
+            Tap the Explore tab to learn more about what's included in this
+            starter app.
+          </Text>
+        </ThemedView>
+        <ThemedView style={styles.stepContainer}>
+          <Text variant="titleLarge">Step 3: Get a fresh start</Text>
+          <Text>
+            When you're ready, run{" "}
+            <Text variant="bodyLarge">npm run reset-project</Text> to get a
+            fresh <Text variant="bodyLarge">app</Text> directory. This will move
+            the current <Text variant="bodyLarge">app</Text> to{" "}
+            <Text variant="bodyLarge">app-example</Text>.
+          </Text>
+        </ThemedView>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -65,6 +60,6 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
 });
---
+++ theme/theme.tsx
@@ -0,0 +1,10 @@
+import { MD3DarkTheme } from "react-native-paper";
+
+export const defaultTheme = {
+  ...MD3DarkTheme,
+  // ? other customization goes here
+  colors: {
+    ...MD3DarkTheme.colors,
+    //? color customization goes here
+  },
+};
